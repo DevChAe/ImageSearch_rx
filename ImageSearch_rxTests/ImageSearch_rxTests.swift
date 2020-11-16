1d329adf52e3ce2715786e550807b465f8bdbc4f
ChAe <competent825@empas.com>
2020-11-16 13:17:00 +0900

    init commit

---
+++ ImageSearch_rxTests/ImageSearch_rxTests.swift
@@ -0,0 +1,33 @@
+//
+//  ImageSearch_rxTests.swift
+//  ImageSearch_rxTests
+//
+//  Created by ChAe on 2020/11/16.
+//
+
+import XCTest
+@testable import ImageSearch_rx
+
+class ImageSearch_rxTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ImageSearch_rx' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ImageSearch_rx
+
+  target 'ImageSearch_rxTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ImageSearch_rxUITests' do
+    # Pods for testing
+  end
+
+pod 'RxCocoa'
+pod 'RxSwift'
+pod 'RxAlamofire'
+pod 'RxKingfisher'
+pod 'SwiftyAttributes'
+
+end
